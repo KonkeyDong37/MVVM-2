21858ad994c652ca9ccf3e8a7ca98ea682ea745a
Andrey <KonkeyDong37@gmail.com>
2020-10-07 00:08:07 +0500

    Initial Commit

---
+++ MVVM-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-2
+//
+//  Created by Андрей on 07.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
